1e16dd132c4bdb72dd8aeecb644f8c8e914f646c
Corey Krewson <ckrewson@aquaveo.com>
2023-12-27 16:04:55 +0000

    tag version v1.0.0_0_2023_12_27

--- setup.py
+++ setup.py
@@ -1,32 +1,30 @@
 from setuptools import setup, find_namespace_packages
-from setup_helper import find_resource_files
+from setup_helper import find_all_resource_files
 
 # -- Apps Definition -- #
-app_package = 'flood_impact_viewer'
-release_package = 'tethysapp-' + app_package
+namespace = 'tethysapp'
+app_package = "flood_impact_viewer"
+release_package = "tethysapp-" + app_package
 
 # -- Python Dependencies -- #
 dependencies = []
 
 # -- Get Resource File -- #
-resource_files = find_resource_files('tethysapp/' + app_package + '/templates', 'tethysapp/' + app_package)
-resource_files += find_resource_files('tethysapp/' + app_package + '/public', 'tethysapp/' + app_package)
-resource_files += find_resource_files('tethysapp/' + app_package + '/workspaces', 'tethysapp/' + app_package)
+resource_files = find_all_resource_files(app_package, namespace)
 
 
-resource_files += find_resource_files('tethysapp/' + app_package + '/scripts', 'tethysapp/' + app_package)
 setup(
     name=release_package,
-    version='1.0.0',
-    description='This app is a viewer app. It displays flood maps and their corresponding imapct statistics tables.',
-    long_description='',
-    keywords=['flood','flood impact','flood damages','flooded areas'],
-    author='Travis Tyler',
-    author_email='travis.tyler8957@gmail.com',
-    url='',
-    license='',
+    version="1.0.0",
+    description="This app is a viewer app. It displays flood maps and their corresponding imapct statistics tables.",
+    long_description="This app is a viewer app. It displays flood maps and their corresponding imapct statistics tables.",
+    keywords="replace_keywords",
+    author="Travis Tyler",
+    author_email="Travis Tyler_email",
+    url="",
+    license="",
     packages=find_namespace_packages(),
-    package_data={'': resource_files},
+    package_data={"": resource_files},
     include_package_data=True,
     zip_safe=False,
     install_requires=dependencies,
